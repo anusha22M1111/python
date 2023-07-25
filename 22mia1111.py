d5d01d4a55546c63f938a0eaf1881d0ff66355c9
anusha22M1111 <127083395+anusha22M1111@users.noreply.github.com>
2023-07-25 09:41:30 +0530

    Add files via upload

---
+++ 22mia1111.py
@@ -0,0 +1,29 @@
+#sample programme
+#22mia1111
+print("hey")
+print(4+3)
+print(4>3)
+a=4
+print(a)
+path="data\"newfolder"
+print(path)
+#a=int(input())
+print(a+50)
+
+#problem 1: calculat pay from basic pay 
+bp=float(input())
+pay=bp+0.8*bp+0.3*bp-0.12*bp
+print(pay)
+
+#problem 2: area of circle
+import math
+r=float(input())
+area=math.pi*r*r
+print(area)
+
+#problem 3: chocolate problem
+bob=int(input())
+rate=10
+wrapper=int(input())
+choco=bob//rate+wrapper//5
+print(choco)
