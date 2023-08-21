6c2392eedf2f46610520b8614fe887daec7d7955
anusha22M1111 <127083395+anusha22M1111@users.noreply.github.com>
2023-08-21 11:31:05 +0530

    Add files via upload

---
+++ practice test-2.py
@@ -0,0 +1,33 @@
+#practice test2
+#22mia1111
+
+#q1-dna sequence
+str=input()
+a=str.count('A')
+g=str.count('G')
+c=str.count('C')
+t=str.count('T')
+d={'A':a,'G':g,'C':c,'T':t}
+print(d)
+
+#Q-2 capitalise first word
+strr=input()
+word=strr.split(" ")
+word.upper()
+print(word)
+
+
+
+#q-3 customer rating
+n=int(input())
+l1=[]
+l2=[]
+l=[[input() for j in range(3)] for i in range(n)]
+for i in range(n):
+    if int(l[i][1])<=20:
+        l1.append(l[i])
+    else:
+        l2.append(l[i])
+print(l)
+print(l1)
+print(l2)
