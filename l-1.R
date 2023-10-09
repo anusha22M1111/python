c333b7a014768c5366231ea8ac1e06e7adce4cef
anusha22M1111 <127083395+anusha22M1111@users.noreply.github.com>
2023-10-09 11:24:54 +0530

    Update l-1.R

--- l-1.R
+++ l-1.R
@@ -65,3 +65,41 @@ ac=h[,3]
 al=h[,c(1,3)]
 ac
 al
+ht=t(h)
+ht
+hc=cbind(h,1:4)
+hc
+hr=rowMeans(h)
+hcm=colMeans(h)
+hr
+hcm
+hr[2]
+hcm[2]
+
+#list
+#creating list of vectors
+v1=c(1,2,3)
+v2=c('aa','bb')
+l=list(v1,v2)
+l
+#indexing
+l[[1]][2]
+l[[2]][1]
+
+#defining a namelist
+l1=list(marks=1:10,names=v2)
+l1
+
+#example-1 of list
+le=list(regno=1:2,name=v2,noc=3:4,marks=c(11,67,89,56,80))
+le
+le1=le[[1]]
+le11=le[[4]]
+le2=le[[2]][1]
+le3=le[[4]][1]
+le[[4]][5]=5+le[[4]][5]
+le1
+le11
+le2
+le3
+le
